010b63f70b361448d419ecc5eab416bbe0105b82
Tim Mattison <tim@mattison.org>
2014-09-25 09:30:03 -0400

    Commented BasicHexChunkProcessor

--- src/main/java/client/dataprocessing/BasicHexChunkProcessor.java
+++ src/main/java/client/dataprocessing/BasicHexChunkProcessor.java
@@ -23,70 +23,71 @@ public class BasicHexChunkProcessor implements HexChunkProcessor {
     }
 
     public List<List<HexChunk>> convert(List<List<BinaryData>> binaryDataList) {
+        // Create maps of the text colors and background colors that is keyed by chunk name
         Map<String, String> backgroundColors = new HashMap<String, String>();
         Map<String, String> textColors = new HashMap<String, String>();
+
+        // Create a list of hex chunk lists (one list per line)
         List<List<HexChunk>> output = new ArrayList<List<HexChunk>>();
 
+        // Loop through the list of binary data (one binary data list per line)
         for (List<BinaryData> binaryDataLine : binaryDataList) {
+            // Convert this line to a hex chunk list
             List<HexChunk> hexChunkList = convertLine(backgroundColors, textColors, binaryDataLine);
 
+            // Add the hex chunk list to the output
             output.add(hexChunkList);
         }
 
         return output;
     }
 
-    public List<HexChunk> convertLine(List<BinaryData> binaryDataLine) {
-        return convertLine(null, null, binaryDataLine);
-    }
-
     private List<HexChunk> convertLine(Map<String, String> backgroundColors, Map<String, String> textColors, List<BinaryData> binaryDataLine) {
-        if (backgroundColors == null) {
-            backgroundColors = new HashMap<String, String>();
-        }
-
-        if (textColors == null) {
-            textColors = new HashMap<String, String>();
-        }
-
+        // Create a list of the output hex chunks
         List<HexChunk> output = new ArrayList<HexChunk>();
 
+        // Loop through the binary data for this line
         for (BinaryData binaryData : binaryDataLine) {
+            // Create two string builders.  One for the text and one for the hex data.
             StringBuilder textStringBuilder = new StringBuilder();
             StringBuilder hexDataStringBuilder = new StringBuilder();
 
+            // Create a new hex chunk
             HexChunk hexChunk = new HexChunk();
 
+            // Put the binary data into the string builders
             innerConvert(binaryData, textStringBuilder, hexDataStringBuilder);
 
+            // Populate the hex chunk object
             hexChunk.hexData = hexDataStringBuilder.toString();
             hexChunk.text = textStringBuilder.toString();
             hexChunk.name = binaryData.name;
 
+            // Does this hex chunk have a name?
             if ((hexChunk.name != null) && (hexChunk.name.length() != 0)) {
+                // Yes, we need to color it
                 boolean moveToNextColor = false;
 
+                // Was a color already selected?
                 if (backgroundColors.containsKey(hexChunk.name)) {
+                    // Yes, use the color we selected already
                     hexChunk.backgroundColor = backgroundColors.get(hexChunk.name);
-                } else {
-                    hexChunk.backgroundColor = colorPicker.getBackgroundColor();
-                    backgroundColors.put(hexChunk.name, hexChunk.backgroundColor);
-                    moveToNextColor = true;
-                }
-
-                if (textColors.containsKey(hexChunk.name)) {
                     hexChunk.textColor = textColors.get(hexChunk.name);
                 } else {
+                    // No, select new colors
+                    hexChunk.backgroundColor = colorPicker.getBackgroundColor();
                     hexChunk.textColor = colorPicker.getTextColor();
-                    textColors.put(hexChunk.name, hexChunk.textColor);
-                    moveToNextColor = true;
-                }
 
-                if (moveToNextColor) {
+                    // Add the colors to the map
+                    backgroundColors.put(hexChunk.name, hexChunk.backgroundColor);
+                    textColors.put(hexChunk.name, hexChunk.textColor);
+
+                    // Move onto the next color
                     colorPicker.nextColor();
                 }
             }
 
+            // Add the hex chunk to the output
             output.add(hexChunk);
         }
 
@@ -94,8 +95,10 @@ public class BasicHexChunkProcessor implements HexChunkProcessor {
     }
 
     private void innerConvert(BinaryData binaryData, StringBuilder textStringBuilder, StringBuilder hexDataStringBuilder) {
+        // Start with no separator
         String separator = null;
 
+        // Loop through each byte of this data
         for (Byte currentByte : binaryData.data) {
             // Get the "real" value of this byte (unsigned)
             int realValue = ((int) currentByte) & 0x0FF;
@@ -113,6 +116,7 @@ public class BasicHexChunkProcessor implements HexChunkProcessor {
             String valueText = HexHelpers.toHexNumberWithLeadingZeroes(realValue, VALUE_LENGTH);
             hexDataStringBuilder.append(valueText);
 
+            // Get the current byte value as an integer
             int currentIntValue = currentByte.intValue();
 
             // Is this a weird character that we can't print easily?
